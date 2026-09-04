7cbe67ffb2c420fe02459efd3da598662d5e2958
KiraboM <kirabomusoke46@gmail.com>
2026-09-04 12:26:14 +0100

    Changed layout of chatbot

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,7 @@ function App() {
 
   async function sendPrompt(message){
     setWaiting(true)
+    setChat(true)
     const response = await fetch('http://localhost:8000/chat',{
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -26,27 +27,51 @@ function App() {
   }, [prompt]) */
 
   return (
-    <div className='main'>
-    
-      <h1>Welcome to your AI Chatbot!</h1>
-      
-      <div className='prompt-field'>
-        <input 
-          type="text"
-          placeholder='Enter promt here' 
-          className='input-field'
-          id='input-field'
-          value={prompt}
-          onChange={(e) => setPrompt(e.target.value)}
-        />
-        <button 
-          className='prompt-btn'
-          onClick={() => sendPrompt(prompt).then((data) => setResponse(data.answer))}
-        >
-            Submit Prompt
-        </button>
-        {waiting ? <p>Waiting for response</p> : <p>{response}</p>}
+    <div>
+      { !inChat ? 
+      <div className='main'>
+        <h1>Welcome to your AI Chatbot!</h1>
+        
+        <div className='prompt-field'>
+          <input 
+            type="text"
+            placeholder='Enter promt here' 
+            className='input-field'
+            id='input-field'
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+          />
+          <button 
+            className='prompt-btn'
+            onClick={() => sendPrompt(prompt).then((data) => setResponse(data.answer))}
+          >
+              Submit Prompt
+          </button>
+        </div>
       </div>
+      : 
+      <div className='chat'>
+        <p className='user-prompt'>{prompt}</p>
+        {waiting ? <p>Waiting for response</p> : <p className='ai-response'>{response}</p>}
+        <div className='prompt-field-chat'>
+          <input 
+            type="text"
+            placeholder='Enter promt here' 
+            className='input-field'
+            id='input-field'
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+          />
+          <button 
+            className='prompt-btn-chat'
+            onClick={() => sendPrompt(prompt).then((data) => setResponse(data.answer))}
+          >
+              Submit Prompt
+          </button>
+        </div>
+
+      </div>
+    }
     </div>
   )
 }
