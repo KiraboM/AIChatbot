33494018a012234ab3bb7db7834aac299c11d8ce
KiraboM <kirabomusoke46@gmail.com>
2026-09-11 14:00:22 +0100

    Fixed positioning of menu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -41,121 +41,75 @@ function App() {
 
   return (
     <div className='entire-screen'>
-      <AnimatePresence>
-        { !menuOpen && <div className='menu-btn-container'>
-          <button
-            className='menu-btn'
-            onClick={() => setMenu(true)}
-          >
-            <img 
-              src={menuIcon} 
-              alt="menuIcon" 
-              width="30"
-              height="30"
-            />
-          </button>
-        </div>
-        }
-      </AnimatePresence>
-      <AnimatePresence>
-        {menuOpen &&
-          <motion.div 
-            className='menu'
-            initial={{x: -100}}
-            animate={{x: 100}}
-            exit={{x: -100}}
-            transition={{ duration: 0.1 }}
-          >
-            <ul className='menu-list'>
-              <li className='menu-li'>New Chat</li>
-              <li className='menu-li'>List of Chats</li>
-            </ul>
-            <button 
-              className='exit-menu-btn'
-              onClick={() => setMenu(false)}
+      <div className='menu-container'>
+        <AnimatePresence>
+          { !menuOpen && <div className='menu-btn-container'>
+            <button
+              className='menu-btn'
+              onClick={() => setMenu(true)}
             >
               <img 
-                src={exitMenu} 
-                alt="exitMenu" 
+                src={menuIcon} 
+                alt="menuIcon" 
                 width="30"
                 height="30"
               />
             </button>
-          </motion.div>
-        }
-      </AnimatePresence>
-      { !inChat ? 
-      <div className='main'>
-        <h1>Welcome to your AI Chatbot!</h1>
-        <div className='prompt-field-container'>
-          <div className='prompt-field'>
-            <input 
-              type="text"
-              placeholder='Enter promt here' 
-              className='input-field'
-              id='input-field'
-              value={prompt}
-              onChange={(e) => setPrompt(e.target.value)}
-              tabIndex="0"
-              onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                  sendPrompt(prompt).then((data) => setResponse(data))
-                }
-              }}
-            />
-            <div className='prompt-btn-chat-container'>
+          </div>
+          }
+        </AnimatePresence>
+        <AnimatePresence>
+          {menuOpen &&
+            <motion.div 
+              className='menu'
+              initial={{x: -100}}
+              animate={{x: 100}}
+              exit={{x: -100}}
+              transition={{ duration: 0.1 }}
+            >
+              <ul className='menu-list'>
+                <li className='menu-li'>New Chat</li>
+                <li className='menu-li'>List of Chats</li>
+              </ul>
               <button 
-                className='prompt-btn-chat'
-                title='Send prompt'
-                onClick={() => sendPrompt(prompt).then((data) => setResponse(data))}
+                className='exit-menu-btn'
+                onClick={() => setMenu(false)}
               >
-                <i className='fa fa-arrow-up w3-large'></i>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-      : 
-        <div className='main'>
-          {conversation.length === 0 ? (
-            <div>
-              <p className='user-prompt'>{prompt}</p>
-              {waiting ? <p>Waiting for resposne...</p> : <p className='ai-response'>{response.answer}</p>}
-            </div>
-          ) : (
-          <div>
-            <ul>
-              {conversation.map((list) => (
-                <li>
-                  <p className='user-prompt' key={list.prompt}>{list.prompt}</p>
-                  <p className='ai-response' key={list.content}>{list.content}</p>
-                </li>
-              ))}
-            </ul>
-            {waiting && (
-              <div>
-                <p className='user-prompt'>{prompt}</p>
-                <p>Waiting for resposne...</p>
-              </div>
-            )}
-          </div>
-          )}
-            <div className='prompt-field-container'>
-              <div className='prompt-field'>
-                <input 
-                  type="text"
-                  placeholder='Enter promt here' 
-                  className='input-field'
-                  id='input-field'
-                  onChange={(e) => setPrompt(e.target.value)}
-                  onKeyDown={(e) => {
-                    if (e.key === "Enter") {
-                      sendPrompt(prompt).then((data) => setResponse(data))
-                    }
-                  }}
+                <img 
+                  src={exitMenu} 
+                  alt="exitMenu" 
+                  width="30"
+                  height="30"
                 />
+              </button>
+            </motion.div>
+          }
+        </AnimatePresence>
+      </div>
+      <div className='chat'>
+        { !inChat ? 
+        <div className='main'>
+          <h1>Welcome to your AI Chatbot!</h1>
+          <div className='prompt-field-container'>
+            <div className='prompt-field'>
+              <input 
+                type="text"
+                placeholder='Enter promt here' 
+                className='input-field'
+                id='input-field'
+                value={prompt}
+                onChange={(e) => setPrompt(e.target.value)}
+                tabIndex="0"
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    sendPrompt(prompt).then((data) => setResponse(data))
+                  }
+                }}
+              />
+              <div className='prompt-btn-chat-container'>
                 <button 
                   className='prompt-btn-chat'
+                  title='Send prompt'
                   onClick={() => sendPrompt(prompt).then((data) => setResponse(data))}
                 >
                   <i className='fa fa-arrow-up w3-large'></i>
@@ -163,8 +117,58 @@ function App() {
               </div>
             </div>
           </div>
-      
-      }
+        </div>
+        : 
+          <div className='main'>
+            {conversation.length === 0 ? (
+              <div>
+                <p className='user-prompt'>{prompt}</p>
+                {waiting ? <p>Waiting for resposne...</p> : <p className='ai-response'>{response.answer}</p>}
+              </div>
+            ) : (
+            <div>
+              <ul>
+                {conversation.map((list) => (
+                  <li>
+                    <p className='user-prompt' key={list.prompt}>{list.prompt}</p>
+                    <p className='ai-response' key={list.content}>{list.content}</p>
+                  </li>
+                ))}
+              </ul>
+              {waiting && (
+                <div>
+                  <p className='user-prompt'>{prompt}</p>
+                  <p>Waiting for resposne...</p>
+                </div>
+              )}
+            </div>
+            )}
+              <div className='prompt-field-container'>
+                <div className='prompt-field'>
+                  <input 
+                    type="text"
+                    placeholder='Enter promt here' 
+                    className='input-field'
+                    id='input-field'
+                    onChange={(e) => setPrompt(e.target.value)}
+                    onKeyDown={(e) => {
+                      if (e.key === "Enter") {
+                        sendPrompt(prompt).then((data) => setResponse(data))
+                      }
+                    }}
+                  />
+                  <button 
+                    className='prompt-btn-chat'
+                    onClick={() => sendPrompt(prompt).then((data) => setResponse(data))}
+                  >
+                    <i className='fa fa-arrow-up w3-large'></i>
+                  </button>
+                </div>
+              </div>
+            </div>
+        
+        }
+      </div>
     </div>
   )
 }
